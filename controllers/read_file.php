3a05ce849b4ebb117fc25ac4eb8f6a3da30cc4a4
Aaron <lazaroo.aaron@lundimatin.fr>
2022-09-06 17:34:51 +0200

    affichage requetes en tableau, format date, correction requetes DELETE

--- controllers/read_file.php
+++ controllers/read_file.php
@@ -7,6 +7,7 @@
    <body>
 
       <?php
+
          $name = $_GET['read'];
 
          $filename = "../uploads/" . $name;
@@ -23,24 +24,65 @@
 
 //          echo ( "Taille : $filesize bytes" );
 //          echo ( "<pre>$filetext</pre>" );
+         // Une requete commence avec select ou delete, on remplace les delete d'abord pour faciliter le tri
+         $filetext = str_replace("\nDELETE","\nSELECT",$filetext);
          $arr = explode("\nSELECT", $filetext);
                   array_shift($arr);
                   $result = array();
                   $keys = array("contenu","date", "utilisateur", "temps");
+                  // Rajout début de la requête enlevé pendant explode()
                   for($i = 0; $i<count($arr); $i++){
-                      array_push($result, "SELECT".$arr[$i]);
+                      $prefix ="";
+                      if(explode(' ',trim($arr[$i]))[0]=="FROM"){
+                        $prefix = "DELETE";
+                      }
+                      else{
+                        $prefix = "SELECT";
+                      }
+                      array_push($result, $prefix.$arr[$i]);
                       $result[$i] = explode("#",$result[$i]);
                       // redefinition clés
                       for($l = 0; $l<4; $l++){
-                         $result[$i][$keys[$l]] = $result[$i][$l];
+                      // format date
+                         if($l==1){
+                            $d = date_create(substr($result[$i][$l],7));
+                            $result[$i][$keys[$l]] = date_format($d, 'Y-m-d H:i:s');
+                         }
+                         else{
+                            $result[$i][$keys[$l]] = $result[$i][$l];
+                         }
                          unset($result[$i][$l]);
                       }
                   }
 
-          echo count($result);
-          echo '<pre>'; print_r($result); echo '</pre>';
-//             echo $result2[1][0];
+          echo "NB REQUETES: ".count($result);
+//           echo '<pre>'; print_r($result); echo '</pre>';
       ?>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>N° Requête</th>
+            <th>Contenu</th>
+            <th>Date</th>
+            <th>Utilisateur</th>
+            <th>Temps</th>
+          </tr>
+        </thead>
+        <tbody>
+      <?php
+      $i=1;
+        foreach ($result as $requetes) {
+            echo '<tr>';
+            echo '<td>' . $i . '</td>';
+            foreach ($requetes as $key1 => $value1) {
+                echo '<td>' . $value1 . '</td>';
+            }
+            $i++;
+            echo '</tr>';
+        }
+       ?>
+      </tbody>
+      </table>
 
    </body>
 </html>
